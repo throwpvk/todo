52c7d6c4ef9fef23a274297c3e8025c9ddf61f2a
Phan Van Khai <215381564+throwpvk@users.noreply.github.com>
2025-12-23 13:39:43 +0900

    fix ip

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const server = http.createServer((req, res) => {
 const PORT = process.env.PORT || 3000;
 
 // Start the server and listen on the specified port
-server.listen(PORT, "localhost", () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
